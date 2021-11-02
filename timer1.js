153f526a95e83ec453a90a69212807567cb0f6c1
tsumitakaoru <Katherine@KT-MacBook-Air-2.local>
2021-11-02 15:39:49 -0400

    add error handle

--- timer1.js
+++ timer1.js
@@ -1,8 +1,21 @@
-const args = process.argv;
-const numbers = args.slice(2);
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  return null;
+}
+let numbers = [];
+for (let index = 0; index < args.length; index++) {
+  if (Number(args[index]) && Number(args[index]) > 0) {
+    numbers.push(Number(args[index]));
+  }
+}
+if (numbers.length === 0) {
+  return null;
+}
+
 numbers.sort(function(a, b) {
   return a - b;
 });
+
 for (let i = 0; i < numbers.length; i++) {
   let ms = numbers[i] * 1000;
   setTimeout(() => {
